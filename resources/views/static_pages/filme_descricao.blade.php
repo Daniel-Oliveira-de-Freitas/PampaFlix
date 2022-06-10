eb25c12cba64ca7909c989b98e4f61a598eca398
CassioJr <cassiojuniorceolin@gmail.com>
2022-06-10 00:40:44 -0300

    melhorias na navbar e criação do descrição filme

---
+++ resources/views/static_pages/filme_descricao.blade.php
@@ -0,0 +1,33 @@
+@extends('fragmentos.main')
+@section('title', 'Descrição filme')
+@section('content')
+    <div class="bg-principal text-white flex  justify-content h-screen">
+        <div class="">
+            <img src="" alt="">
+            <h1>Pontuação do IMDB:</h1>
+        </div>
+        <div>
+            <div  class="text-xl">
+                <h1>Nome:</h1>
+                <h1>Duração:</h1>
+                <h1>Data lançamento:</h1>
+                <h1>Sinopse:</h1>
+            </div>
+                <p> Lorem ipsum taciti ornare justo aliquam velit curabitur urna quisque, pharetra quis eget odio semper
+                    iaculis
+                    quis pellentesque eu, vitae malesuada congue tempor facilisis aliquam at aenean. egestas blandit velit
+                    neque
+                    justo rutrum duis fusce libero, morbi rhoncus scelerisque quis molestie malesuada habitasse, tellus
+                    bibendum
+                    ligula sed eu convallis consequat. lacus egestas odio potenti posuere etiam himenaeos tempor dolor
+                    potenti,
+                    luctus arcu lacinia praesent metus platea tristique posuere, lectus metus nisi malesuada interdum a
+                    aptent
+                    augue. hendrerit volutpat sapien vulputate consequat dapibus est massa iaculis, eleifend urna fringilla
+                    id
+                    interdum purus nec torquent, aliquam torquent varius euismod pharetra vulputate ipsum. </p>
+                <p>Qual a sua avaliação para o filme?</p>
+        </div>
+        <h1>Favoritar</h1>
+    </div>
+@endsection
